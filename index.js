7594a0a43e0deced8f71e621944837a894cad6f7
raviranve <raviranve786@gmail.com>
2025-05-28 00:03:24 +0530

    add origin

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://property-deal-frontend-rf5w.vercel.app",
   credentials: true, // Allow cookies
 };
 app.use(cors(corsOptions));
