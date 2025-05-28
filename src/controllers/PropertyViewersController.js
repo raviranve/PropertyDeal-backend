63735214d9392c79a7e9b833e29a1afb582ded5b
raviranve <raviranve786@gmail.com>
2025-05-28 21:59:58 +0530

    fix viewrs count

--- src/controllers/PropertyViewersController.js
+++ src/controllers/PropertyViewersController.js
@@ -4,10 +4,32 @@ const View = require("../models/Viewers");
 const { success } = require("../utils/responseHandler");
 const sendSms = require("../utils/SendSms");
 
+const getClientIp = (req) => {
+  const forwarded = req.headers["x-forwarded-for"];
+  return forwarded ? forwarded.split(",")[0].trim() : req.socket.remoteAddress;
+};
+
+// exports.trackViewers = async (req, res) => {
+//   try {
+//     const ipAddress =
+//       req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+//     const existing = await View.findOne({ ipAddress });
+
+//     if (!existing) {
+//       await View.create({ ipAddress });
+//       sendSms("New Visitor tracked from Website", `IP: ${ipAddress}`);
+//     }
+
+//     res.status(200).json({ message: "View tracked" });
+//   } catch (err) {
+//     res.status(500).json({ message: "Error tracking view", error: err });
+//   }
+// };
+
 exports.trackViewers = async (req, res) => {
   try {
-    const ipAddress =
-      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const ipAddress = getClientIp(req);
 
     const existing = await View.findOne({ ipAddress });
 
